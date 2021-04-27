89ead8852d813963deff4d5dd2e282f3b9292d2f
Raybook90 <ratanak.hy90@gmail.com>
2021-04-27 19:02:13 +0200

    function to fetch blocktime and insert rows to database

--- fetch_blocktime_and_insert_to_db.py
+++ fetch_blocktime_and_insert_to_db.py
@@ -1,20 +1,44 @@
-from db_populate import insertBlocktimeIntoTable
-from api.Bitcoin_Ethereum import get_blocktime
-from api.Stellar import get_blocktime_stellar
-from api.EOS import get_blocktime_eos
-from api.Stratis import get_blocktime_stratis
-from api.NEO import get_blocktime_neo
-from api.Cardano import get_blocktime_cardano
-from api.Ripple import get_blocktime_ripple
-from api.QTUM import get_blocktime_qtum
-from api.ICON import get_blocktime_icon
-from api.VeChain import get_blocktime_vechain
-from api.Wanchain import get_blocktime_wanchain
+from api_blocktime.Bitcoin_Ethereum import get_blocktime
+from api_blocktime.Stellar import get_blocktime_stellar
+from api_blocktime.EOS import get_blocktime_eos
+from api_blocktime.Stratis import get_blocktime_stratis
+from api_blocktime.NEO import get_blocktime_neo
+from api_blocktime.Cardano import get_blocktime_cardano
+from api_blocktime.Ripple import get_blocktime_ripple
+from api_blocktime.QTUM import get_blocktime_qtum
+from api_blocktime.ICON import get_blocktime_icon
+from api_blocktime.VeChain import get_blocktime_vechain
+from api_blocktime.Wanchain import get_blocktime_wanchain
 import schedule
 import time
+import sqlite3
 
 
-def insert_rows_every_12mins():
+def insertBlocktimeIntoTable(name, time):
+    try:
+        sqliteConnection = sqlite3.connect('bcio.db')
+        cursor = sqliteConnection.cursor()
+        # print("Successfully connected to database")
+
+        sqlite_insert_query = '''INSERT INTO blocktime(blockchain, blocktime)
+        VALUES(?,?)'''
+
+        data = (name, time)
+        cursor.execute(sqlite_insert_query, data)
+        sqliteConnection.commit()
+        # print("Record inserted successfully into attributes_for_dataset table")
+
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Failed to insert data ino sqlite table", error)
+    finally:
+        if(sqliteConnection):
+            sqliteConnection.close()
+            # print("The SQLite connection is closed")
+
+
+def insert_rows():
     insertBlocktimeIntoTable('Bitcoin', get_blocktime('Bitcoin'))
     insertBlocktimeIntoTable('Ethereum', get_blocktime('Ethereum'))
     insertBlocktimeIntoTable('Stellar', get_blocktime_stellar())
@@ -29,8 +53,9 @@ def insert_rows_every_12mins():
     print('Rows have successfully been added to the dataset')
 
 
-# insert_rows_every_12mins()
-schedule.every(11).minutes.do(insert_rows_every_12mins)
+insert_rows()
+# insert rows every 11 minutes
+schedule.every(11).minutes.do(insert_rows)
 
 while True:
     schedule.run_pending()
